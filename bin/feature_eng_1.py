2a80f9f9796211ae2c9c1da628e6066377d8fed0
mslovett21 <patrycja.krawczuk@gmail.com>
2020-05-27 09:22:04 -0700

    fixes eng 1

--- bin/feature_eng_1.py
+++ bin/feature_eng_1.py
@@ -39,11 +39,9 @@ def dataframe_setup(train,cols):
 
     return data_matrix
 
-def add_date_block(test, data,cols):
+def add_date_block(test, data):
     test["date_block_num"] = 34
     test["date_block_num"] = test["date_block_num"].astype(np.int8)
-    data = pd.concat([data, test.drop(["ID"],axis = 1)], ignore_index=True, sort=False, keys=cols)
-    data.fillna( 0, inplace = True )
     return test, data
 
 
@@ -55,8 +53,13 @@ def monthly_sales_count(train, data,cols):
     data["item_cnt_month"] = data["item_cnt_month"].fillna(0).clip(0,20).astype(np.float16)
 
     return data
-
-def merge_dataframes(data, categories, items):
+'''
+Creates final version of the main dataframe (in terms of rows)
+to which later features (columns) are added 
+'''
+def merge_dataframes(data, test,cols,categories, items):
+    data = pd.concat([data, test.drop(["ID"],axis = 1)], ignore_index=True, sort=False, keys=cols)
+    data.fillna( 0, inplace = True )
     data = pd.merge( data, items, on = ["item_id"], how = "left")
     data = pd.merge( data, categories, on = ["item_category_id"], how = "left" )
     data.drop(columns= ["item_name","item_category_name"], inplace = True)
@@ -71,9 +74,10 @@ def main():
     
     cols            = ["date_block_num", "shop_id", "item_id"]
     main_data       = dataframe_setup(train, cols)
-    main_data       = monthly_sales_count(train, main_data, cols)
-    test, main_data = add_date_block(test,main_data, cols)
-    
+    main_data       = monthly_sales_count(train, main_data,cols)
+    test, main_data = add_date_block(test,main_data)
+    main_data       = merge_dataframes(main_data, test, cols,categories,items)
+
     pickle.dump(main_data, open('main_data_feature_eng_1.pickle', 'wb'), protocol = 4)
     pickle.dump(test, open('test_data_feature_eng_1.pickle', 'wb'), protocol = 4)
 
