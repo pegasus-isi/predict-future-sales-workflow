dc7f97262795e30926f2f848ebcdaa597c5b771d
mslovett21 <patrycja.krawczuk@gmail.com>
2020-06-01 12:17:51 -0700

    adds more features

--- bin/feature_eng_4.py
+++ bin/feature_eng_4.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 import pandas as pd
 import pickle
-
+import numpy as np
+from IPython import embed
 """
 Creates holiday features with 3 lags from future months.
 	
@@ -54,12 +55,79 @@ def create_all_holiday_features(train, holidays):
     
     return main_data
 
+def create_featrues_last_sale_shop(matrix,train):
+    tmp_list=[]
+    for mid in matrix['date_block_num'].unique():
+        tmp=train.loc[train['date_block_num']<mid,['date_block_num','shop_id','item_id']].groupby(['shop_id','item_id']).last().rename({'date_block_num':'item_month_id_of_last_sale_in_shop'},axis=1).astype(np.int16)
+        tmp['date_block_num']=mid
+        tmp.reset_index(inplace=True)
+        tmp_list.append(tmp) 
+    tmp = pd.concat(tmp_list)
+
+    matrix = matrix.join(tmp.set_index(['date_block_num','shop_id','item_id']),on=['date_block_num','shop_id','item_id'])
+    # time since last sale in shop
+    matrix['item_months_since_last_sale_in_shop']=matrix['date_block_num']-matrix['item_month_id_of_last_sale_in_shop']
+
+    return matrix
+
+def create_featrues_month_last_sale(matrix,train):
+    tmp_list=[]
+    for mid in matrix['date_block_num'].unique():
+        tmp=train.loc[train['date_block_num']<mid,['date_block_num','item_id']].groupby('item_id').last().rename({'date_block_num':'item_month_id_of_last_sale'},axis=1)
+        tmp['date_block_num']=mid
+        tmp.reset_index(inplace=True)
+        tmp_list.append(tmp)      
+    tmp = pd.concat(tmp_list)
+
+    matrix=matrix.join(tmp.set_index(['date_block_num','item_id']),on=['date_block_num','item_id'])
+    # downcast dtype (int not possible due to NaN values)
+    matrix['item_month_id_of_last_sale']=matrix['item_month_id_of_last_sale'].astype(np.float16)
+    # time since last sale over all shops
+    matrix['item_months_since_last_sale']=matrix['date_block_num']-matrix['item_month_id_of_last_sale']
+    # downcast dtype (int not possible due to NaN values)
+    matrix['item_month_id_of_last_sale']=matrix['item_month_id_of_last_sale'].astype(np.float16)
+    # time since last sale over all shops
+    matrix['item_months_since_last_sale']=matrix['date_block_num']-matrix['item_month_id_of_last_sale']
+
+    return matrix
+
+def create_opening_relase_related_features(matrix,train):
+    matrix['shop_months_since_opening']=matrix['date_block_num']-matrix['shop_id'].map(matrix[['date_block_num','shop_id']].groupby('shop_id').min()['date_block_num'])
+    matrix['shop_opening']=(matrix['shop_months_since_opening']==0)
+    matrix['item_month_id_of_release']=matrix['item_id'].map(matrix[['date_block_num','item_id']].groupby('item_id').min()['date_block_num'])
+    matrix['item_month_of_release']=matrix['item_month_id_of_release']%12+1
+    matrix['item_months_since_release']=matrix['date_block_num']-matrix['item_month_id_of_release']
+    matrix['item_months_since_release'].clip(0,12,inplace=True)  
+    matrix['item_new']=(matrix['item_months_since_release']==0)
+    # month where the item has first been sold in shop
+    group=train[['date_block_num','shop_id','item_id']].groupby(['shop_id','item_id']).min().rename({'date_block_num':'item_month_id_of_first_sale_in_shop'},axis=1)
+    matrix = pd.merge(matrix, group, on=['shop_id', 'item_id'], how='left')
+    matrix['item_month_of_first_sale_in_shop']=matrix['item_month_id_of_first_sale_in_shop']%12+1
+
+    # number of months since the item has been released in this shop
+    matrix['item_months_since_first_sale_in_shop']=(matrix['date_block_num']-matrix['item_month_id_of_first_sale_in_shop'])
+    matrix['item_months_since_first_sale_in_shop'].clip(0,12,inplace=True)  # group together items sold for more than a year in the shop
+
+    # whether the item has already been sold in this shop before or not
+    matrix['item_never_sold_in_shop_before']=~(matrix['item_months_since_first_sale_in_shop']>0)
+
+    # set month of release and number of months since the item has been released in this shop to -1 for all items never sold in shop before (remove info from future)
+    matrix.loc[matrix['item_never_sold_in_shop_before'],'item_months_since_first_sale_in_shop']=-1
+    matrix.loc[matrix['item_never_sold_in_shop_before'],'item_month_id_of_first_sale_in_shop']=-1
+    matrix.loc[matrix['item_never_sold_in_shop_before'],'item_month_of_first_sale_in_shop']=-1
+
+    matrix['item_never_sold_in_shop_before_but_not_new']=((1-matrix['item_new'])*matrix['item_never_sold_in_shop_before']).astype(bool)
+    matrix['item_seniority']=(2-matrix['item_new'].astype(int)-matrix['item_never_sold_in_shop_before'].astype(int)).astype(np.int8)
+    return matrix
 
 def main():
     holidays    = pd.read_csv("holidays.csv")
     train       = pd.read_pickle("main_data_feature_eng_1.pickle")
-    main_data   = create_holiday_lags(train, holidays)
+    main_data   = create_all_holiday_features(train, holidays)
     main_data   = create_first_sale_features(main_data)
+    main_data   = create_opening_relase_related_features(main_data,train)
+    main_data   = create_featrues_last_sale_shop(main_data,train)
+    mian_data   = create_featrues_month_last_sale(main_data,train)
     pickle.dump(main_data, open("main_data_feature_eng_4.pickle", "wb"), protocol = 4)
 
 
--- bin/feature_eng_5.py
+++ bin/feature_eng_5.py
@@ -10,20 +10,19 @@ from IPython import embed
 		'categories_preprocessed_0.pickle'
 		'shops_preprocessed_0.pickle'
 		'items_preprocessed_0.pickle'
-                'sales_train.csv'
+        "sales_train.csv"
 
 	FILES OUT: 
 		'main_data_feature_eng_5.pickle'
  """
 
-def create_price_lag_feature(main_data, feature_name, group_by_features, train, aggragation_opt_dict):
+def create_price_lag_feature(main_data,feature_name, group_by_features,train, aggragation_opt_dict):   
     group = train.groupby(group_by_features).agg(aggragation_opt_dict)
     group.columns = feature_name
-    group.reset_index(inplace=True)   
-    main_data = main_data.merge(group, on=group_by_features, how="left")
+    group.reset_index(inplace = True)   
+    main_data = main_data.merge( group, on = group_by_features, how = "left" )    
     
     return main_data
-
 
 def add_lag_feature(df, lags, main_cols, cols):
     for col in cols:
@@ -33,12 +32,12 @@ def add_lag_feature(df, lags, main_cols, cols):
             shifted = tmp.copy()
             shifted.columns = main_cols + [ col + "_lag_"+str(i)]
             shifted.date_block_num = shifted.date_block_num + i
-            df = pd.merge(df, shifted, on=main_cols, how="left")
+            df = pd.merge(df, shifted, on=main_cols, how ="left")    
     
     return df
 
 
-def create_single_delta_lag(flag,matrix, lags, features_to_drop, delta_feature_name, base_feature_name):
+def create_single_delta_lag(flag,matrix, lags,features_to_drop,delta_feature_name, base_feature_name):
     def select_trends1(row) :
         for i in lags:
             if row["delta_price_lag_" + str(i)]:
@@ -54,18 +53,18 @@ def create_single_delta_lag(flag,matrix, lags, features_to_drop, delta_feature_n
     else:
         matrix[delta_feature_name] = matrix.apply(select_trends2, axis = 1)        
     
-    matrix[delta_feature_name].fillna(0 ,inplace=True)
+    matrix[delta_feature_name].fillna( 0 ,inplace = True)
 
     for i in lags:
-        features_to_drop.append(base_feature_name + "_lag_" + str(i))
-        features_to_drop.append(delta_feature_name + "_" + str(i))
+        features_to_drop.append( base_feature_name + "_lag_" + str(i) )
+        features_to_drop.append(delta_feature_name + "_" + str(i) )
 
-    matrix.drop(features_to_drop, axis=1, inplace=True)
+    matrix.drop(features_to_drop, axis = 1, inplace = True)
     
     return matrix
 
 
-def create_delta_lag_features(flag, matrix, lags,main_cols, delta_feature_name, base_feature_name, overave_feature_name):
+def create_delta_lag_features(flag, matrix,lags,main_cols, delta_feature_name, base_feature_name, overave_feature_name):   
     matrix = add_lag_feature(matrix, lags, main_cols, [base_feature_name])
     
     for i in lags:
@@ -73,26 +72,39 @@ def create_delta_lag_features(flag, matrix, lags,main_cols, delta_feature_name, 
         (matrix[base_feature_name +"_lag_" + str(i)]- matrix[overave_feature_name] )/ matrix[overave_feature_name]
     
     features_to_drop = [overave_feature_name, base_feature_name]
-    matrix = create_single_delta_lag(flag,matrix,lags, features_to_drop,delta_feature_name, base_feature_name)
+    matrix           = create_single_delta_lag(flag,matrix,lags, features_to_drop,delta_feature_name, base_feature_name)    
     
     return matrix
 
+def create_all_delta_features(main_data):
+    lags_3 = [1,2,3]
+    lags_1 = [1]
+    main_cols  = ["date_block_num", "shop_id", "item_id"]
+
+    main_data = create_delta_lag_features(1,main_data,lags_3,main_cols, "delta_price_lag", "date_item_avg_item_price", "item_avg_item_price")    
+    main_data = create_delta_lag_features(2,main_data,lags_3,main_cols, "delta_shop_price_lag", "date_shop_item_avg_item_price", "shop_item_avg_item_price") 
+    main_data["delta_revenue"] = (main_data['date_shop_revenue'] - main_data['shop_avg_revenue']) / main_data['shop_avg_revenue']
+    main_data                  = add_lag_feature(main_data, lags_1, main_cols,["delta_revenue"])
+    main_data.drop( ["date_shop_revenue", "shop_avg_revenue", "delta_revenue"] ,axis = 1, inplace = True)
+    
+    return main_data
+
 
 def create_all_price_related_lags(main_data, train):
+    
     train["revenue"]          = train["item_cnt_day"] * train["item_price"]
 
     lags_3     = [1, 2, 3]
     lags_1     = [1]
-    main_cols  = ["date_block_num", "shop_id", "item_id"]
     
     agg_dict_itemp_mean      = {"item_price": ["mean"]}   
-    agg_dict_revenue_sum     = {"revenue": ["sum"]}     
-    agg_dict_date_mean       = {"date_block_num": ["mean"]}      
+    agg_dict_revenue_sum     = {"revenue": ["sum"] }     
+    agg_dict_date_mean       = {"date_block_num":["mean"] }      
     
     group_by_item            = ["item_id"]
     group_by_shop            = ["shop_id"]
-    group_by_month_item      = ["date_block_num", "item_id"]
-    group_by_month_shop      = ["date_block_num", "shop_id"]
+    group_by_month_item      = ["date_block_num","item_id"]
+    group_by_month_shop      = ["date_block_num","shop_id"]
     group_by_shop_item       = ["shop_id", "item_id"]   
     group_by_month_shop_item = ["date_block_num", "shop_id", "item_id"]    
     
@@ -103,34 +115,24 @@ def create_all_price_related_lags(main_data, train):
     revenue_feature_shop      = ["shop_avg_revenue"]    
     price_feature_month_shop_item = ["date_shop_item_avg_item_price"]
     
-    main_data = create_price_lag_feature(main_data, price_feature_item_ave, group_by_item, train, agg_dict_itemp_mean)
-    main_data = create_price_lag_feature(main_data, price_feature_month_ave, group_by_month_item, train,agg_dict_itemp_mean)    
-    
-    main_data = create_delta_lag_features(1, main_data,lags_3, main_cols, "delta_price_lag", "date_item_avg_item_price", "item_avg_item_price")    
-    
-    main_data = create_price_lag_feature(main_data, revenue_feature_date_shop, group_by_month_shop, train, agg_dict_revenue_sum)    
-    main_data = create_price_lag_feature(main_data, revenue_feature_shop, group_by_shop, train, agg_dict_date_mean)  
-    
-    main_data["delta_revenue"] = (main_data["date_shop_revenue"] - main_data["shop_avg_revenue"]) / main_data["shop_avg_revenue"]
-    main_data                  = add_lag_feature(main_data, lags_1, main_cols,["delta_revenue"])
-    main_data.drop(["date_shop_revenue", "shop_avg_revenue", "delta_revenue"] ,axis = 1, inplace=True)
-    
-    main_data    = create_price_lag_feature(main_data, price_feature_shop_item, group_by_shop_item, train, agg_dict_itemp_mean)
-    main_data    = create_price_lag_feature(main_data, price_feature_month_shop_item, group_by_month_shop_item, train, agg_dict_itemp_mean)  
-    
-    main_data    = create_delta_lag_features(2, main_data, lags_3, main_cols, "delta_shop_price_lag", "date_shop_item_avg_item_price", "shop_item_avg_item_price")        
-
+    main_data = create_price_lag_feature(main_data,price_feature_item_ave,group_by_item,train ,agg_dict_itemp_mean)
+    main_data = create_price_lag_feature(main_data,price_feature_month_ave,group_by_month_item,train,agg_dict_itemp_mean)       
+    main_data = create_price_lag_feature(main_data,revenue_feature_date_shop ,group_by_month_shop,train,agg_dict_revenue_sum)    
+    main_data = create_price_lag_feature(main_data,revenue_feature_shop,group_by_shop, train, agg_dict_date_mean)     
+    main_data = create_price_lag_feature(main_data,price_feature_shop_item,group_by_shop_item,train,agg_dict_itemp_mean)
+    main_data = create_price_lag_feature(main_data,price_feature_month_shop_item,group_by_month_shop_item,train,agg_dict_itemp_mean)  
+         
     return main_data
-
-
+    
 def main():
-    main_data  = pd.read_pickle("main_data_feature_eng_1.pickle")
-    items      = pd.read_pickle("items_preprocessed_0.pickle")
-    categories = pd.read_pickle("categories_preprocessed_0.pickle")
-    shops      = pd.read_pickle("shops_preprocessed_0.pickle")
+    main_data  = pd.read_pickle('main_data_feature_eng_1.pickle')
+    items      = pd.read_pickle('items_preprocessed_0.pickle')
+    categories = pd.read_pickle('categories_preprocessed_0.pickle')
+    shops      = pd.read_pickle('shops_preprocessed_0.pickle')
     org_train  = pd.read_csv("sales_train.csv")
 
     main_data = create_all_price_related_lags(main_data,org_train)
+    main_data = create_all_delta_features(main_data)
     pickle.dump(main_data, open("main_data_feature_eng_5.pickle", "wb"), protocol = 4)
 
 
