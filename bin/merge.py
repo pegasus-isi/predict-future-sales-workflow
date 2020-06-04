7e3e83ad261cf58b49fedbc7783f41684bfcc107
George Papadimitriou <georgpap@isi.edu>
2020-06-03 20:51:36 -0700

    split train and test

--- bin/merge.py
+++ bin/merge.py
@@ -17,6 +17,9 @@ import pandas as pd
 		'train_group_0.pickle'
                 'train_group_1.pickle'
                 'train_group_2.pickle'
+                'test_group_0.pickle'
+                'test_group_1.pickle'
+                'test_group_2.pickle'
                 'main_data_feature_eng_all.pickle'
  """
 
@@ -63,14 +66,25 @@ def main():
     #main_data_merged = merge_dataframes(main_data_merged, threeNN_shops, ["shop_id"])
 
     #split dataframe to groups based on seniority
-    train_group_0 = get_train_0(main_data_merged)
-    train_group_1 = get_train_1(main_data_merged)
-    train_group_2 = get_train_2(main_data_merged)
+    group_0 = get_train_0(main_data_merged)
+    group_1 = get_train_1(main_data_merged)
+    group_2 = get_train_2(main_data_merged)
 
+    train_group_0 = group_0[group_0.date_block_num < 34]
+    train_group_1 = group_1[group_1.date_block_num < 34]
+    train_group_2 = group_2[group_2.date_block_num < 34]
+
+    test_group_0 = group_0[group_0.date_block_num == 34]
+    test_group_1 = group_1[group_1.date_block_num == 34]
+    ttest_group_2 = group_2[group_2.date_block_num == 34]
+    
     #save output
     pickle.dump(train_group_0, open("train_group_0.pickle", "wb"), protocol=4)
     pickle.dump(train_group_1, open("train_group_1.pickle", "wb"), protocol=4)
     pickle.dump(train_group_2, open("train_group_2.pickle", "wb"), protocol=4)
+    pickle.dump(test_group_0, open("test_group_0.pickle", "wb"), protocol=4)
+    pickle.dump(test_group_1, open("test_group_1.pickle", "wb"), protocol=4)
+    pickle.dump(test_group_2, open("test_group_2.pickle", "wb"), protocol=4)
     pickle.dump(main_data_merged, open("main_data_feature_eng_all.pickle", "wb"), protocol=4)
 
 
